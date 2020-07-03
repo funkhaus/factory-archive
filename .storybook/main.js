512a3f16fed2f2ebd7249268b98b8fb4169451d4
James Lee <uptownhr@gmail.com>
2020-07-02 22:21:38 -0700

    removed knobs, fixed story naming

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,3 @@
 module.exports = {
-    stories: ["../src/**/*.stories.[tj]s"],
-    addons: ['@storybook/addon-knobs/register']
+    stories: ["../src/**/*.stories.[tj]s"]
 }
--- src/components/BannerHighlight/BannerHighlight.stories.js
+++ src/components/BannerHighlight/BannerHighlight.stories.js
@@ -8,7 +8,7 @@ export default {
     decorators: [withKnobs]
 }
 
-export const BlockWorkDefault = () => ({
+export const Default = () => ({
     components: { BannerHighlight },
     data() {
         return {
@@ -18,7 +18,7 @@ export const BlockWorkDefault = () => ({
     template: `<banner-highlight :image="api.image" to="/" />`
 })
 
-export const BlockWorkWithTitle = () => ({
+export const WithTitle = () => ({
     components: { BannerHighlight },
     data() {
         return {
@@ -28,7 +28,7 @@ export const BlockWorkWithTitle = () => ({
     template: `<banner-highlight text="Sample" :image="api.image" to="/" />`
 })
 
-export const BlockWorkWithPrompt = () => ({
+export const WithPrompt = () => ({
     components: { BannerHighlight },
     data() {
         return {
@@ -36,18 +36,4 @@ export const BlockWorkWithPrompt = () => ({
         }
     },
     template: `<banner-highlight prompt="Sample" :image="api.image" to="/" />`
-})
-
-export const BlockWorkPlayground = () => ({
-    components: { BannerHighlight },
-    props: {
-        prompt: { default: text('Prompt', 'Hello') },
-        text: { default: text('Title', 'helloo2') }
-    },
-    data() {
-        return {
-            api: API
-        }
-    },
-    template: `<banner-highlight :prompt="prompt" :text="text" :image="api.image" to="/" />`
 })
