ae0367b9864f15fbede1ce51c9eccd465a469aee
derek chiampas <derek.chiampas@gmail.com>
2020-07-06 15:03:25 -0700

    renames default story

--- src/components/PanelSidetray/PanelSidetray.stories.js
+++ src/components/PanelSidetray/PanelSidetray.stories.js
@@ -1,26 +1,26 @@
-import PanelSidetray from "./PanelSidetray";
-import API from "@/static/db.json";
+import PanelSidetray from "./PanelSidetray"
+import API from "@/static/db.json"
 
 export default {
     title: "@dchiamp / PanelSidetray"
-};
+}
 
-export const Panel = () => ({
+export const Default = () => ({
     components: { PanelSidetray },
     data() {
         return {
             api: API
-        };
+        }
     },
     template: `<panel-sidetray  :items="api.pages"/>`
-});
+})
 
 export const PanelActiveItem = () => ({
     components: { PanelSidetray },
     data() {
         return {
             api: API
-        };
+        }
     },
     template: `<panel-sidetray  :items="api.pages" :active-index="1"/>`
-});
+})
