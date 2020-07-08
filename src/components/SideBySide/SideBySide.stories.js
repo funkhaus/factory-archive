0542656b78fa746b4e86ada17453b8a913d03a5b
derek chiampas <derek.chiampas@gmail.com>
2020-07-08 14:23:45 -0700

    adds single side-by-side example

--- src/components/SideBySide/SideBySide.stories.js
+++ src/components/SideBySide/SideBySide.stories.js
@@ -78,3 +78,20 @@ export const Mobile = () => ({
     },
     template: `<side-by-side  :items="items" :isMobile="true"/>`
 })
+
+export const Single = () => ({
+    components: { SideBySide },
+    data() {
+        return {
+            api: API,
+            items: [
+                {
+                    image: API.images[0],
+                    text:
+                        "<p>We&#8217;re rescuing ya. Who said that? SURE you can die! You want to die?! Pansy. I didn&#8217;t ask for a completely reasonable excuse! I asked you to get busy! Meh.</p>\n<p>Too much work. Let&#8217;s burn it and say we dumped it in the sewer. Incidentally, you have a dime up your nose. Doomsday device? Ah, now the ball&#8217;s in Farnsworth&#8217;s court! I&#8217;ve been there. My folks were always on me to groom myself and wear underpants. What am I, the pope?</p>\n<p>Why am I sticky and naked? Did I miss something fun? No, of course not. It was… uh… porno. Yeah, that&#8217;s it. Who are those horrible orange men? Fry! Quit doing the right thing, you jerk! I decline the title of Iron Cook and accept the lesser title of Zinc Saucier, which I just made up. Uhh… also, comes with double prize money.</p>\n<p>We&#8217;ll need to have a look inside you with this camera. Dear God, they&#8217;ll be killed on our doorstep! And there&#8217;s no trash pickup until January 3rd. But, like most politicians, he promised more than he could deliver.</p>\n"
+                }
+            ]
+        }
+    },
+    template: `<side-by-side  :items="items" />`
+})
